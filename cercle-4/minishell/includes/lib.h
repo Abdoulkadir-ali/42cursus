c23d340ebceee3e700b4c11af432ed8ddccef163
Abdoulkadir Ali <abdoali@f1br8s20.clusters.42paris.fr>
2026-01-15 04:50:19 +0100

    Includes norminette

--- cercle-4/minishell/includes/lib.h
+++ cercle-4/minishell/includes/lib.h
@@ -6,7 +6,7 @@
 /*   By: abdoali <abdoali@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/13 03:25:06 by abdoali           #+#    #+#             */
-/*   Updated: 2026/01/14 17:46:42 by abdoali          ###   ########.fr       */
+/*   Updated: 2026/01/15 04:49:41 by abdoali          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,6 @@
 
 // CUSTOM
 # include "libft.h"
-
 
 int		ft_strmatch(char *s1, char *s2, int (*match)(char *, char *));
 int		str_any(char *s1, char *s2);
