ec9baf37e39a9447c9a185f3347624862571842e
Abdoulkadir Ali <abdoali@f1br6s19.clusters.42paris.fr>
2026-01-26 05:00:51 +0100

    Error fixes

--- cercle-4/minishell/srcs/exec/builtins/pwd.c
+++ cercle-4/minishell/srcs/exec/builtins/pwd.c
@@ -6,7 +6,7 @@
 /*   By: abdoali <abdoali@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/11 13:00:00 by abdoali           #+#    #+#             */
-/*   Updated: 2026/01/26 04:19:04 by abdoali          ###   ########.fr       */
+/*   Updated: 2026/01/26 04:55:52 by abdoali          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,17 @@
 int	ft_pwd(char **envp)
 {
 	char	cwd[1024];
+	char	*pwd;
 
-	(void)envp;
 	if (getcwd(cwd, sizeof(cwd)))
 	{
 		ft_putendl_fd(cwd, 1);
+		return (0);
+	}
+	pwd = get_env_val_simple("PWD", envp);
+	if (pwd)
+	{
+		ft_putendl_fd(pwd, 1);
 		return (0);
 	}
 	perror("pwd");
